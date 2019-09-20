ec85c429d848b31a1890157dfa3025a431922f45
Anupam <angodse@ncsu.edu>
2019-09-20 02:41:24 +0000

    * Csv format corrected

--- HW1/log_loads.sh
+++ HW1/log_loads.sh
@@ -22,12 +22,12 @@ do
   #populate log file	
   timestamp=$(date)
   load_avgs=$(uptime | grep -o 'load.*' | cut -d ':' -f 2)
-  echo $timestamp $load_avgs >> $HOME/load.csv
 
-  #Generate alerts
   la1=$(echo $load_avgs | cut -d ',' -f 1)
   la5=$(echo $load_avgs | cut -d ',' -f 2)
   la15=$(echo $load_avgs | cut -d ',' -f 3)
+
+  echo $timestamp,$la1,$la5,$la15 >> $HOME/load.csv
 
   if [ $(echo "$la1 > $last_la1" | bc) -eq 1 ] && [ $(echo "$last_la1 != -1" | bc) -eq 1 ]; then
     increasing=1
@@ -38,11 +38,11 @@ do
   last_la1=$la1
 
   if [ $( echo "$la1 > $X" | bc ) -eq 1 ]; then
-    echo $timestamp "HIGH CPU usage" $load_avgs >> $HOME/load_alerts.csv
+    echo $timestamp,"HIGH CPU usage",$la1,$la5,$la15 >> $HOME/load_alerts.csv
   fi
 
   if [ $( echo "$la5 > $Y" | bc ) -eq 1 ] && [ $increasing -eq 1 ]; then
-    echo $timestamp "Very HIGH CPU usage" $load_avgs >> $HOME/load_alerts.csv
+    echo $timestamp,"Very HIGH CPU usage",$la1,$la5,$la15 >> $HOME/load_alerts.csv
   fi
     
   #wait for $T secs

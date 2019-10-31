88eb30092925365dbc8873c14f5792442a54b63c
Yash Kamdar <ykamdar@zaloni.com>
2019-10-31 15:24:19 -0400

    updated test scripts

--- HW3-yash/q1-py-virt/README.md
+++ HW3-yash/q1-py-virt/README.md
@@ -10,14 +10,14 @@ but my approach would fetch even the static IPs set in the guest VM.
 Steps to run:
 
 Local hypervisor:
-python resolve_conflict.py
+sudo python resolve_conflict.py
 
 For Remote hypervisors:
 Make sure to have the ssh private key in your system that can access the hypervisor
-e.g. python resolve_conflict.py ece792@192.168.122.71,user@anotherIP
+e.g. sudo python resolve_conflict.py ece792@192.168.122.71,user@anotherIP
 
 
 
 Side note:
-Script to reset MAC address of a VM
-python test_resetMac.py VM2-yk eth1 52:54:00:02:02:02
+Script to reset MAC address of a VM (Wait for 20 seconds before running the resolve conflict script)
+sudo python test_resetMac.py VM1-yk eth1 52:54:00:11:8b:fd 52:54:00:9a:49:9b
--- HW3-yash/q1-py-virt/test_resetMac.py
+++ HW3-yash/q1-py-virt/test_resetMac.py
@@ -5,11 +5,11 @@ import xml.etree.ElementTree as ET
 import time
 import random
 
-def resetMac(domName,iface,curr_mac):
+def resetMac(domName,iface,curr_mac,new_mac):
     dom = conn.lookupByName(domName)
     dom_xml = dom.XMLDesc(0)
     #new_mac="52:54:00:%02x:%02x:%02x" % (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
-    new_mac="52:54:00:11:8b:fd"
+    #new_mac="52:54:00:11:8b:fd"
     xml_root = ET.XML(dom_xml)
     interfaces = xml_root.find('devices').findall('interface')
     for interface in interfaces:
@@ -28,5 +28,5 @@ def resetMac(domName,iface,curr_mac):
         dom.create()
 
 conn = libvirt.open('qemu:///system')
-resetMac(sys.argv[1],sys.argv[2],sys.argv[3])
+resetMac(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
 #resetMac('VM2-yk','eth1','52:54:00:4b:ad:6d')

8694db14133c9cc704a3fad545753c69b331f4eb
Yash Kamdar <ykamdar@zaloni.com>
2019-10-30 23:21:04 -0400

    h3q1p2

--- HW3-yash/q1-py-virt/README.md
+++ HW3-yash/q1-py-virt/README.md
@@ -1,4 +1,10 @@
-Ensure Guest-agent is installed in all VMs, if not then install using
+Steps to run:
+python p1.py
+python p2.py
+
+
+
+Notes:Ensure Guest-agent is installed in all VMs, if not then install using
 yum install qemu-guest-agent
 
 Verify from Hypervisor:
@@ -6,3 +12,4 @@ virsh qemu-agent-command VM1-yk '{"execute":"guest-network-get-interfaces"}'
 
 Another approach would be to fetch the IP from the dnsmasq leases file on the hypervisor (/var/lib/misc/dnsmasq.leases),
 but my approach would fetch even the static IPs set in the guest VM.
+
--- HW3-yash/q1-py-virt/p1.py
+++ HW3-yash/q1-py-virt/p1.py
@@ -1,20 +1,61 @@
 # Listing all MACs and IPs of VMs
 import sys
 import libvirt
+import xml.etree.ElementTree as ET
+import time
+import random
+
+def resetMac(domName,iface,curr_mac):
+    dom = conn.lookupByName(domName)
+    dom_xml = dom.XMLDesc(0)
+    new_mac="52:54:00:%02x:%02x:%02x" % (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+
+    xml_root = ET.XML(dom_xml)
+    interfaces = xml_root.find('devices').findall('interface')
+    for interface in interfaces:
+        mac_tag = interface.find('mac')
+        if mac_tag.attrib['address'] == curr_mac:
+            mac_tag.attrib['address']=new_mac
+    #xml_tree.write('/etc/libvirt/qemu/'+dom.name()+'.xml')
+    xml_str=ET.tostring(xml_root)
+    dom_file=open("/etc/libvirt/qemu/"+dom.name()+".xml",'wb')
+    dom_file.write(xml_str)
+    dom_file.close()
+    dom.destroy()
+    conn.defineXML(xml_str)
+    if dom.isActive() != True: 
+        dom.create()
+
+def resolveConflicts(VMinfo):
+    for vm in sorted (VMinfo.keys()) :
+        interfaces=VMinfo[vm]['interfaces']
+        for iface in interfaces.keys():
+            curr_mac=interfaces[iface]['mac_address']
+            if curr_mac in MACs:
+                new_mac=resetMac(VMinfo[vm]['name'],iface,curr_mac)
+                interfaces[iface]['mac_address']=new_mac
+            else:
+                MACs.append(curr_mac)
 
 def printInfo(VMinfo):
     for vm in sorted (VMinfo.keys()) :  
         print(VMinfo[vm]['name'])
-        for iface in VMinfo[vm]['name'].keys():
-            print(iface,VMinfo[vm][iface])
+        interfaces=VMinfo[vm]['interfaces']
+        for iface in interfaces.keys():
+            print(iface,interfaces[iface])
 
 def loadInfo(domainID):
     dom = conn.lookupByID(domainID)
     currInfo=dict()
     interfaces=dict()
     domName=dom.name()
-
-    ifaces = dom.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT, 0)
+    
+    ifaces=[]
+    try:
+        ifaces = dom.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT, 0)
+    except:
+        #backup in case agent is not installed
+        ifaces = dom.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE)
 
     for (name, val) in ifaces.iteritems():
         mac_address=val['hwaddr']
@@ -34,7 +75,10 @@ def loadInfo(domainID):
     currInfo['interfaces']=interfaces
     VMinfo[domainID]=currInfo
 
+def libvirt_callback(userdata, err):
+    pass
 
+libvirt.registerErrorHandler(f=libvirt_callback, ctx=None)
 conn = libvirt.open('qemu:///system')
 if conn == None:
     print('Failed to open connection to qemu:///system')
@@ -45,9 +89,8 @@ MACs=[]
 if domainIDs == None:
     print('Failed to get a list of domain IDs')
 for dom_id in domainIDs:
-    try:
-        loadInfo(dom_id)
-    except Exception as e:
-        pass
+    loadInfo(dom_id)
+printInfo(VMinfo)
+resolveConflicts(VMinfo)
 printInfo(VMinfo)
 
